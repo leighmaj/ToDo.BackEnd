d963d1e80e5d11fbebce9720f3b19eee20bb579b
leighmaj <51490479+leighmaj@users.noreply.github.com>
2020-01-10 17:16:30 -0600

    Added Comment Controller and Model

---
+++ ToDo.BackEnd/Controllers/CommentsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LiteDB;
+using ToDo.BackEnd.Models;
+
+namespace ToDo.BackEnd.Controllers
+{
+	public class CommentsController : ApiController
+	{
+
+		[HttpPost]
+		[Route("api/comments")]
+		public IHttpActionResult CreateNewComment(Comment newComment)
+		{
+			using (var db = new LiteDatabase(@"C:\ToDo\ToDo.db"))
+			{
+				var commentCollection = db.GetCollection<Comment>("Comments");
+
+				var taskCollection = db.GetCollection<Task>("Tasks");
+
+				var userCollection = db.GetCollection<User>("Users");
+
+				var foundUser = userCollection.FindById(newComment.UserId);
+
+				if (foundUser == null)
+				{
+					return BadRequest();
+				}
+
+				var foundTask = taskCollection.FindById(newComment.TaskId);
+
+				if (foundTask == null)
+				{
+					return BadRequest();
+				}
+
+				newComment.Id = Guid.NewGuid();
+
+				newComment.CreatedDate = DateTime.Now;
+
+				commentCollection.Insert(newComment);
+
+				return Ok(newComment);
+			}
+		}
+	}
+}
---
+++ ToDo.BackEnd/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.BackEnd.Models
+{
+	public class Comment
+	{
+		public Guid Id { get; set; }
+		public Guid UserId { get; set; }
+		public Guid TaskId { get; set; }
+		public string CommentBody { get; set; }
+		public DateTime CreatedDate { get; set; }
+	}
+	
+}
